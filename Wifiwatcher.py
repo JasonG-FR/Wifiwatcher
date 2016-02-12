c4a0dc9bdf24aa394ffcd076b4a003a5b5466995
JasonG-FR <jason.gombert@gmail.com>
2016-02-12 16:08:24 +0100

    Repassage en mode test sur le RPi

--- Wifiwatcher.py
+++ Wifiwatcher.py
@@ -110,20 +110,23 @@ def main(args):
                 #Téléchargement et compilation du package
                 nom = buildPkg(user)
                 #Installation du package
-                installPkg(nom)
+                """installPkg(nom)"""
+                print("s1")
                             
         #Sinon on télécharge et compile le dernier
         else:
             #Téléchargement et compilation du package
             nom = buildPkg(user)
             #Installation du package
-            installPkg(nom)
+            """installPkg(nom)"""
+            print("s2")
 
     else:
         if pkgExiste():
             #Installer le package disponible
             nom = subprocess.check_output(["ls 8188eu-dkms/*.pkg.tar.xz"], shell=True).decode("utf8")
-            installPkg(nom)
+            """installPkg(nom)"""
+            print("s3")
             
         else:
             print("La carte Wifi est désactivée et aucun package de 8188eu-dkms disponible localement...")
@@ -141,10 +144,11 @@ def main(args):
             #Téléchargement et compilation du package
             nom = buildPkg(user)
             #Installation du package
-            installPkg(nom)
+            """installPkg(nom)"""
+            print("s4")
             
         #Redémarrer système dans 1 minute (pour pouvoir quitter le script et sauvegarder les logs!)
-        subprocess.check_output(["shutdown -r +1 'Redémarrage système imminent pour réactivation du Wifi...'"], shell=True)
+        """subprocess.check_output(["shutdown -r +1 'Redémarrage système imminent pour réactivation du Wifi...'"], shell=True)"""
         print("---- Reboot ----")
             
     return 0

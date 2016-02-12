23060b3fc5750c253ee2b57f32b6ed29b894f048
JasonG-FR <jason.gombert@gmail.com>
2016-02-12 13:40:51 +0100

    Détection de la présence de l'interface wifi

--- Wifiwatcher.py
+++ Wifiwatcher.py
@@ -22,8 +22,30 @@
 #  
 #  
 
+import subprocess
 
 def main(args):
+    #Pour rappel, le script est lancé avec les droits root !
+    #On vérifie si la carte Wifi est reconnue
+    
+    interface = "wlan0"
+
+    try:
+        subprocess.check_output(["ifconfig " + interface], shell=True)
+        print(interface + "connectée !")
+        interface_OK = True
+    except subprocess.CalledProcessError:
+        print(interface + " non connectée !")
+        interface_OK = False
+    
+    if interface_OK:
+        #Vérifier que le package existe
+        
+            #Vérifier qu'il est à jour
+    else:
+        #Installer le package disponible
+        #Redémarrer système
+    
     return 0
 
 if __name__ == '__main__':
